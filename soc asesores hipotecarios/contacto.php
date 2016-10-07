b5196cbf7908162903457bfba701da49fc999783
angelgurrola <angel_gurrolac@outlook.com>
2016-10-06 21:21:27 -0500

    Cambios en textos
    Cambios en textos y en dirección de correo electrónico.

--- soc asesores hipotecarios/contacto.php
+++ soc asesores hipotecarios/contacto.php
@@ -6,32 +6,8 @@ $mensaje = $_POST['mensaje'];
 
 
 
-
-$para = 'angel_gurrolac@outlook.com';
-$titulo = 'Prueba de mensaje';
-$header = 'From: ' . $email;
-$msjCorreo = "Nombre: $nombre\n E-Mail: $email\n Telefono: $telefono\n Mensaje:\n $mensaje";
-
-if ($_POST['submit']) {
-if (mail($para, $titulo, $msjCorreo, $header)) {
-echo "<script language='javascript'>
-alert('Mensaje enviado, muchas gracias.');
-window.location.href = 'http://socfluye.com/';
-</script>";
-} else {
-echo 'Fall¨® el envio';
-}
-<?php
-$nombre = $_POST['nombre'];
-$email = $_POST['correo'];
-$telefono = $_POST['telefono'];
-$mensaje = $_POST['mensaje'];
-
-
-
-
-$para = 'socfluye-durango@hotmail.com';
-$titulo = 'Prueba de mensaje';
+$para = 'contacto@socfluye.com';
+$titulo = 'Mensaje de la página socfluye';
 $header = 'From: ' . $email;
 $msjCorreo = "Nombre: $nombre\n E-Mail: $email\n Telefono: $telefono\n Mensaje:\n $mensaje";
 
